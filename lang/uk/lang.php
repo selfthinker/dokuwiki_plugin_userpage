e068a9efeca821863e8d4119d3744f3c457cb0b3
Yaroslav <zevs91@gmail.com>
2023-12-08 09:25:45 +0100

    translation update

--- lang/uk/lang.php
+++ lang/uk/lang.php
@@ -1,10 +1,9 @@
 <?php
+
 /**
  * Ukrainian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  */
-
-$lang['userpage']        = 'Сторінка користувача';
-
-//Setup VIM: ex: et ts=2 :
+$lang['userpage']              = 'Сторінка користувача';
